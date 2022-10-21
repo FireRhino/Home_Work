94495bfb6b054bf64e5c82d9e56c4b5e4cf42a97
FireRhino <113386849+FireRhino@users.noreply.github.com>
2022-10-21 14:01:41 +0300

    Add files via upload

---
+++ HW8.2.c
@@ -0,0 +1,25 @@
+#define _USE_MATH_DEFINES
+#define CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+float m(float x)
+{float y;
+y = ((pow(x, 3)) + (3 * pow(x, 2)) - 3);
+return y;
+}
+int main()
+{
+	float q, t;
+	float x=2;
+	setlocale(LC_ALL, "RUS");
+	printf("¬ведите шаг табул€ции: ");
+	scanf("%f", &q);
+	printf("| x   |   f(x)|\n");
+	t = ((pow(x, 3)) + (3 * pow(x, 2)) - 3);
+	for (; x<=4; x+=q) 
+	{
+		printf("| %.2f | %.2f | \n", x, m(x));
+	}
+}
