93601c27b77595456ea47e1d5a747e1cd76431fb
FireRhino <113386849+FireRhino@users.noreply.github.com>
2022-10-25 13:00:28 +0300

    Add files via upload

---
+++ HW 8.1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int m, n, s=0, t=0;
+	setlocale(LC_ALL, "RUS");
+	puts("Введите два числа: ");
+	scanf("%d", &m);
+	scanf("%d", &n);
+	for (int i = m; i <= n; i++)
+	{
+		s=s+i;
+		t=t+1;
+		printf("Выполнено %d раз\n", t);
+	}
+	printf("Результат %d\n", s);
+	return 0;
+}
